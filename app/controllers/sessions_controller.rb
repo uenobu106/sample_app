8b923ec94c32e959c93a9b78332e9844c0cd95a4
uenobu106 <uenobu106@ezweb.ne.jp>
2018-01-02 11:01:56 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:session][:password])
         # ユーザーログイン後にユーザー情報のページにリダイレクトする
         log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_to user
       else
         # flash[:danger] = 'Invalid email/password combination' # 本当は正しくない
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,35 @@
 class UsersController < ApplicationController
-  def index
 
-  end
+  # GET /users/:id
   def show
     @user = User.find(params[:id])
-    #=>app/view
+    # => app/views/users/show.html.erb
+    # debugger
   end
 
   def new
     @user = User.new
+    # => form_for @user
   end
 
+  # POST /users
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save # => Validation
+      # Sucess
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
-      # redirect_to user_path(@user.id)
-      # redirect_to user_path(@user)
-      #GET => /users/#{@user.id} => show
-      # 保存の成功をここで扱う。
+      # GET "/users/#{@user.id}" => show
     else
+      # Failure
       render 'new'
     end
   end
 
-  private
-    def user_params
-      params.require(:user).permit(
+  def user_params
+    params.require(:user).permit(
       :name, :email, :password,
       :password_confirmation)
-    end
+  end
 end
