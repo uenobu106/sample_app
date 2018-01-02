c19f2d9bed9536be6fbe78c07a5d5b6b54c3a648
uenobu106 <uenobu106@ezweb.ne.jp>
2018-01-02 16:37:03 +0900

    finsh ch11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save # => Validation
-      # Sucess
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
       # GET "/users/#{@user.id}" => show
     else
       # Failure
